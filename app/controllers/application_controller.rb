e0d170f62a62fad68e64cf3adef5a6d124b33722
Matt Hinz <matt.hinz@gsa.gov>
2022-03-17 14:05:17 -0700

    Use helper rather than instance var for pages list
    Add a helper to pull the list of pages available.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,3 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
-
-  before_action :look_up_pages
-
-  private
-
-  def look_up_pages
-    @pages = Page.build_hierarchy Rails.root.join "_pages"
-  end
 end
