2f4aafe7ac81ca64558f57566a9dc46e27f9320c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-03-22 16:11:08 -0400

    Add tests for OmniauthController

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "pages/home.html.erb", type: :view do
-  xit "displays the gov banner" do
+  it "displays the gov banner" do
     render template: "pages/home", layout: "layouts/application"
     expect(rendered).to match "An official website of the United States government"
   end
