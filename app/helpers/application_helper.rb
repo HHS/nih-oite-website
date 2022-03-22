afd39d1e20b81d1053641dee531e30ac3eb7685f
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-03-22 10:30:07 -0400

    Remove unused i18n code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
-  def format_active_locale(locale_string)
-    link_classes = "usa-nav__link"
-    if locale_string.to_sym == I18n.locale
-      link_classes = "#{link_classes} usa-current"
-    end
-    link_to t("shared.languages.#{locale_string}"), root_path(locale: locale_string), class: link_classes
-  end
-
   def pages
     @pages ||= Page.build_hierarchy Rails.root.join "_pages"
   end
