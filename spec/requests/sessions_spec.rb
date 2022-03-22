6a263532e31b781d3b81e013ba87e6c77beccab3
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-03-22 15:16:57 -0400

    Add tests for SessionsController

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,7 +1,45 @@
 require "rails_helper"
 
 RSpec.describe "Sessions", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:user) { create :user, :cms }
+
+  describe "POST /.netlify/identity/token" do
+    context "guest user" do
+      it "returns an error message" do
+        post "/.netlify/identity/token"
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq({message: "User must be logged in first"}.to_json)
+      end
+    end
+    context "cms user" do
+      before { sign_in user }
+      it "returns a JWT token" do
+        post "/.netlify/identity/token"
+        expect(response).to have_http_status(:success)
+        token, _header = JWT.decode(JSON.parse(response.body)["access_token"], Rails.application.credentials.jwt_secret)
+        expect(token["email"]).to eq user.email
+        expect(token["exp"]).to be_kind_of(Integer)
+        expect(token["app_metadata"]["roles"]).to eq ["cms"]
+      end
+    end
+  end
+
+  describe "GET /.netlify/identity/user" do
+    context "guest user" do
+      it "returns an error message" do
+        get "/.netlify/identity/user"
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.body).to eq({message: "User must be logged in first"}.to_json)
+      end
+    end
+    context "cms user" do
+      before { sign_in user }
+      it "returns the current user's details" do
+        get "/.netlify/identity/user"
+        expect(response).to have_http_status(:success)
+        body = JSON.parse(response.body)
+        expect(body["email"]).to eq user.email
+      end
+    end
   end
 end
