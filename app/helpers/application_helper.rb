36f4faf1c470ba62f532b7a099d3e4400c12305a
Matt Hinz <matt.hinz@gsa.gov>
2022-03-23 10:57:03 -0700

    Add settings for footer
    - New "Settings" collection that's just .yml files in _settings
    - Model + view helper for `Footer` settings
    - USWDS "slim" footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
+require "yaml"
+
 module ApplicationHelper
   def pages
     @pages ||= Page.build_hierarchy Rails.root.join "_pages"
+  end
+
+  def footer
+    @footer ||= Footer.load
   end
 end
---
+++ app/models/footer.rb
@@ -0,0 +1,51 @@
+require "yaml"
+
+# Model wrapping a blob of YAML data describing settings related to the global site footer.
+class Footer
+  class Link
+    attr_reader :url, :text
+    def initialize(url, text)
+      @url = url
+      @text = text
+    end
+  end
+
+  attr_reader :data
+
+  def self.load(file = Rails.root.join("_settings", "footer.yml"))
+    data = YAML.safe_load File.read(file)
+    new data
+  end
+
+  def initialize data
+    @data = data
+  end
+
+  def agency_email
+    @data["agency_email"]
+  end
+
+  def agency_name
+    @data["agency_name"]
+  end
+
+  def agency_phone
+    @data["agency_phone"]
+  end
+
+  def links
+    unless @links
+      @links = []
+      (1..5).each do |i|
+        l = @data["link#{i}"]
+        next if l.nil?
+
+        if l["url"] != "" && l["text"] != ""
+          link = Footer::Link.new l["url"], l["text"]
+          @links.push link
+        end
+      end
+    end
+    @links
+  end
+end
