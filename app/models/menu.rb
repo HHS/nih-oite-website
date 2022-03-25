1e8834e23acfee93e95493843f80045f5332a9dc
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-03-25 13:18:39 -0400

    Refactor Menu::Item for more rubyisms

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,34 +1,33 @@
 # A Menu is a view into a hierarchy of pages.
 class Menu
   class Item
+    attr_reader :page, :text
+    delegate :public?, to: :page
+
     def initialize(page, text = nil, include_children = false)
       @page = page
-      @text = text
+      @text = text || page.title
       @include_children = include_children
     end
 
+    def include_children?
+      !!@include_children
+    end
+
     def children
-      @children ||= if @include_children
-        @page.children.map { |child| Menu::Item.new child }
+      @children ||= if include_children?
+        page.children.map { |child| Menu::Item.new child }
       else
         []
       end
     end
 
     def has_children?
-      @include_children && @page.has_children?
+      include_children? && page.has_children?
     end
 
     def path
-      @page.filename
-    end
-
-    def public?
-      @page.public?
-    end
-
-    def text
-      @text || @page.title
+      page.filename
     end
   end
 
