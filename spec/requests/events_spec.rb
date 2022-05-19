002b367e93b1c15c811b4d5b73819eabe451e5f5
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-05-19 15:10:41 -0400

    Handle missing events files with 404 response

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -16,5 +16,11 @@ RSpec.describe "Events", type: :request do
       get "/events/202257-training-event"
       expect(response).to render_template :show
     end
+
+    it "returns a 404 error when the event isn't found" do
+      expect {
+        get "/events/202211-notfound"
+      }.to raise_error(ActionController::RoutingError)
+    end
   end
 end
